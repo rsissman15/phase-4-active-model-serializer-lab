c9a24c0d575ec40bf5eaf03e99df6470ccdd31c3
Rebecca Sissman <rsissman15@aol.com>
2022-08-01 15:05:10 -0400

    completed Assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,12 +3,12 @@ class AuthorsController < ApplicationController
 
   def index
     authors = Author.all 
-    render json: authors
+    render json: authors, include: ['profile', 'post.tags']
   end
 
   def show
     author = Author.find(params[:id])
-    render json: author
+    render json: author, include: ['profile', 'post.tags']
   end
 
   private
